3c5966b1494ca07f5478dcb0243af6226cd2885a
geert1981 <56909236+geert1981@users.noreply.github.com>
2025-09-23 10:35:27 +0200

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,15 @@
 // main.js - centrale entrypoint voor de app
 // Importeer alle globale functies en hang ze aan window zodat ze vanuit HTML bereikbaar zijn
-
-import { show_div } from 'https://geert1981.github.io/Results/scripts/script_10_show_div.js';
-import { ical } from 'https://geert1981.github.io/Results/scripts/script_13_iCal.js';
-import { stats_team, stats_club, stats_reeks, stats } from 'https://geert1981.github.io/Results/scripts/script_14_links.js';
-import { youtube, instagram, linkedin, facebook, x_twitter, tiktok } from 'https://geert1981.github.io/Results/scripts/script_14_links.js';
-import { scroll_team_buttons } from 'https://geert1981.github.io/Results/scripts/script_11_scroll_up_down.js';
-import { toggle_teams } from 'https://geert1981.github.io/Results/scripts/script_12_toggle_teams.js';
-import { change_club } from 'https://geert1981.github.io/Results/scripts/script_1_info_clubs.js';
-import { show_previous_week, show_current_week, show_next_week } from 'https://geert1981.github.io/Results/scripts/script_8_kalender_reeks.js';
-import { update_leden } from 'https://geert1981.github.io/Results/scripts/script_4_leden.js';
+import { change_club } from './script_1_info_clubs.js';
+import { filter_table_leden } from './script_2_sort_filter.js';
+import { update_leden } from './script_4_leden.js';
+import { show_previous_week, show_current_week, show_next_week } from './script_8_kalender_reeks.js';
+import { show_div } from './script_10_show_div.js';
+import { scroll_team_buttons } from './script_11_scroll_up_down.js';
+import { toggle_teams } from './script_12_toggle_teams.js';
+import { ical } from './script_13_iCal.js';
+import { stats_team, stats_club, stats_reeks, stats } from './script_14_links.js';
+import { youtube, instagram, linkedin, facebook, x_twitter, tiktok } from './script_14_links.js';
 
 // Koppel functies aan window voor gebruik in onclick etc.
 window.show_div = show_div;
@@ -31,9 +31,11 @@ window.show_previous_week = show_previous_week;
 window.show_current_week = show_current_week;
 window.show_next_week = show_next_week;
 window.update_leden = update_leden;
+window.filter_table_leden = filter_table_leden;
 
 // Voeg hier eventueel extra initialisatie toe
 // ...
+
 
 
 
